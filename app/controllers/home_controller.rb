626e5cb848672ea4dcff1091c637e2d1b4e73de5
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2019-11-02 21:34:36 -0300

    add show only 15 Houses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @houses = House.all
+    @houses = House.all.take(15)
     @houses_premium = House.where(premium: true).take(5)
   end
 end
